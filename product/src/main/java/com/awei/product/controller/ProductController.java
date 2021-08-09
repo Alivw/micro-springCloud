5143cc23edbd4ce14f1790f2e9d5caa773343283
shizuwei <shizuwei99@gmail.com>
2021-08-09 17:41:14 +0800

    2020/08/09 shizuwei Product查询功能

---
+++ product/src/main/java/com/awei/product/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.awei.product.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author shizuwei
+ * @since 2021-08-09
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+}
---
+++ product/src/main/java/com/awei/product/service/IProductService.java
@@ -0,0 +1,16 @@
+package com.awei.product.service;
+
+import com.awei.product.entity.Product;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author shizuwei
+ * @since 2021-08-09
+ */
+public interface IProductService extends IService<Product> {
+
+}
