21fa0410f24224213fd6618b43bd98e60e0d400b
shi zuwei <shizuwei99@gmail.com>
2021-08-15 18:11:20 +0800

    2021/08/13 shizuwei Hystrix Dashboard 安装

--- order/src/main/java/com/awei/order/OrderApplication.java
+++ order/src/main/java/com/awei/order/OrderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class OrderApplication {
 
     public static void main(String[] args) {
